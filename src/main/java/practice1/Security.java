f7dcd8b046f4d5212c460dc38591e917f62746c4
guanyingjie <39229309+guanyingjie@users.noreply.github.com>
2019-05-21 15:00:06 +0800

    finish practice 01

--- src/main/java/practice1/Security.java
+++ src/main/java/practice1/Security.java
@@ -1,3 +1,45 @@
+//package practice1;
+//
+//import com.google.common.collect.ImmutableList;
+//
+//public class Security {
+//
+//
+//    private SecurityChecker securityChecker;
+//
+////
+//    public Security(SecurityChecker checker) {
+//        this.securityChecker = checker;
+//    }
+//    public boolean hasAccess(User user,Permission permission,ImmutableList<Permission>permissions){
+//        return (securityChecker.isAdmin())||(this.securityChecker.checkPermission(user,permission)||permissions.contains(permission));
+//    }
+////    public boolean hasAccess(User user, Permission permission, ImmutableList<Permission> permissions) {
+////
+////        boolean isAccess = false;
+////        if (user == null) {
+////            return isAccess;
+////        }
+////
+////        if (permission == null) {
+////            return isAccess;
+////        }
+////
+////        if (permissions.size() == 0) {
+////            return isAccess;
+////        }
+////
+////        if (securityChecker.isAdmin()) {
+////            isAccess = true;
+////        }
+////
+////        if (this.securityChecker.checkPermission(user, permission) || permissions.contains(permission)) {
+////            isAccess = true;
+////        }
+////
+////        return isAccess;
+////    }
+//}
 package practice1;
 
 import com.google.common.collect.ImmutableList;
@@ -12,27 +54,8 @@ public class Security {
 
     public boolean hasAccess(User user, Permission permission, ImmutableList<Permission> permissions) {
 
-        boolean isAccess = false;
-        if (user == null) {
-            return isAccess;
-        }
-
-        if (permission == null) {
-            return isAccess;
-        }
-
-        if (permissions.size() == 0) {
-            return isAccess;
-        }
-
-        if (securityChecker.isAdmin()) {
-            isAccess = true;
-        }
-
-        if (this.securityChecker.checkPermission(user, permission) || permissions.contains(permission)) {
-            isAccess = true;
-        }
-
-        return isAccess;
+        return securityChecker.isAdmin() ||
+                securityChecker.checkPermission(user, permission) ||
+                permissions.contains(permission);
     }
 }
